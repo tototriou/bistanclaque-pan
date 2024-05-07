b6e4092678d185a1bfc1d9386a36fbf6d2bde03c
Thomas Triouleyre <thomas.triouleyre@kuhn.com>
2024-05-07 16:58:54 +0200

    to be fixed

--- website/src/app/components/modal-presentation-membres/modal-presentation-membres.component.ts
+++ website/src/app/components/modal-presentation-membres/modal-presentation-membres.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, Input, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-modal-presentation-membres',
@@ -8,6 +8,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './modal-presentation-membres.component.html',
   styleUrl: './modal-presentation-membres.component.scss',
   encapsulation: ViewEncapsulation.None,
+  imports: [NgStyle],
 
 })
 export class ModalPresentationMembresComponent {
